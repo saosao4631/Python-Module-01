4a15750946a35fcb5343ff9cc71d678f3ef58a8e
ksaotome <ksaotome@student.42tokyo.jp>
2026-05-31 21:13:49 +0900

    aaa

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,9 @@
+def ft_garden_intro():
+    print("=== Welcome to My Garden ===")
+    print("Plant: Rose")
+    print("Height: 25cm")
+    print("Age: 30 days")
+    print("=== End of Program ===")
+
+if __name__ == "__main__":
+    ft_garden_intro()
